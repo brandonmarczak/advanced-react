89150cb2861fdc0ee13ecee55782863ab1db4ba6
Brandon <brandonmarczak@yahoo.co.uk>
2020-10-21 22:43:04 +0100

    :recycle: Setting up Prisma, GraphQL Yoga server and creating items

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,22 @@
-const mutations = {};
+const Mutation = {
+    async createItem(parent, args, ctx, info) {
+        // TODO: Check if they are logged in
+        const item = ctx.db.mutation.createItem({
+            data: {
+                ...args
+            }
+        }, info);
+        return item;
+    }
+    // createDog(parent, args, ctx, info) {
+    //     global.dogs = global.dogs || [];
+    //     // create a dog
+    //     const newDog = {name: args.name};
+    //     global.dogs.push(newDog);
+    //     return newDog;
+    //     // create a dog!
+    //     console.log(args);
+    // }
+};
 
-module.exports = mutations;
+module.exports = Mutation;
